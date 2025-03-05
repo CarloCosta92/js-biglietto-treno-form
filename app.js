e9ab66cda38900fbc7b78bce5b0db813cd7fb655
Carlo Costa <carlo.costa92@gmail.com>
2025-03-05 15:25:50 +0100

    feat: solved milestone 1

--- app.js
+++ app.js
@@ -6,23 +6,52 @@
 // - va applicato uno sconto del 20% per i minorenni
 // - va applicato uno sconto del 40% per gli over 65.
 
+// id bottone e funzione al click per attivare la funzione del cacolo biglietto
+const button = document.getElementById('buttonCalc');
+button.addEventListener('click', calcoloBiglietto);
+
 
 // creare una function
 
 function calcoloBiglietto (){
 // prendere i dati da input
 
+let km = document.getElementById('userDistance').value;
+let age = document.getElementById('userAge').value;
+
 // convertire input in numero
+
+km = parseFloat(km);
+age = parseInt(age);
+
+console.log(km)
 
 //calcolo prezzo base
 
+const price= parseFloat(0.21);   
+
+let priceTicket = price * km;
+
 //definizione sconti
+
+const discountYoung = 0.80;
+const discountOld = 0.60;
 
 //condizione per calcolo
 
+if (age < 18) {
+    priceTicket= priceTicket * discountYoung;
+   
+} else if(age > 65){
+   priceTicket = priceTicket * discountOld;
+}
+
 // riduzione cifre del biglietto ad al massimo due decimali
 
-// stampa costo del biglietto
+priceTicket = priceTicket.toFixed(2);
 
-
+console.log("Il prezzo del biglietto ferroviario ammonta a " + priceTicket +" €");
 }
+
+
+
